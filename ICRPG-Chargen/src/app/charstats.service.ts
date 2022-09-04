d9e0d41e8ae6d8f0c10700fbf9ed981e1035027f
Zane Ives <deni23n@gmail.com>
2022-09-04 16:18:56 +0200

    syncing

--- ICRPG-Chargen/src/app/charstats.service.ts
+++ ICRPG-Chargen/src/app/charstats.service.ts
@@ -10,15 +10,7 @@ export class CharstatsService {
   storyName: string = "";
   charLifeform: string = "";
   charType: string = "";
-  
-  // statHeading = ["Name", "Base", "Lifeform", "Loot"];
-  // strStat = ["Str",0,0,0];
-  // dexStat = ["Dex",0,0,0];
-  // conStat = ["Con",0,0,0];
-  // intStat = ["Int",0,0,0];
-  // wisStat = ["Wis",0,0,0];
-  // chaStat = ["Cha",0,0,0];
-
+ 
   strStat = {
     name: "Str",
     base: 0,
@@ -56,13 +48,6 @@ export class CharstatsService {
     loot: 0
   }
 
-  // effHeading = ["Name", "Base", "Lifeform", "Loot"]
-  // basicEff = ["Basic", 0, 0, 0];
-  // weaponEff = ["Weapons & Tools", 0, 0, 0];
-  // gunEff = ["Guns", 0, 0, 0];
-  // magicEff = ["Energy and Magic", 0, 0, 0];
-  // ultiEff = ["Ultimate", 0, 0, 0];
-
   basicEff = {
     name: "Basic",
     base: 0,
@@ -98,27 +83,16 @@ export class CharstatsService {
   heroCoin = false;
   deathTimer = 0;            
   
-  // carryName: string = "";
-  // isEquipped: boolean = false;
-  // carryDesc: string = "";
-  // carryInteraction?: string = "";
-  
   carryName = ["","","","","","","","","","","","","","","","","","","",""]
   isEquipped = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
   carryDesc = ["","","","","","","","","","","","","","","","","","","",""]
   carryInteraction = ["","","","","","","","","","","","","","","","","","","",""]
 
-  abilityType: string = "";
-  abilityName: string = "";
-  abilityDesc: string = "";
-  abilityInteraction: string = "";
+  abilityType = ["","","","","","","","","","","","","","","",""]
+  abilityName = ["","","","","","","","","","","","","","","",""]
+  abilityDesc = ["","","","","","","","","","","","","","","",""]
+  abilityInteraction = ["","","","","","","","","","","","","","","",""]
+
   constructor () {  }
 
 }
-
-// CharInfo
-// CharStatus
-// CharStats
-// CharEffort
-// CharInventory
-// CharAbility
